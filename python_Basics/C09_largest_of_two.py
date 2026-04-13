1523882695b8c9a866c7ec556744a9e60b5f368f
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-13 14:24:30 +0530

    Code 9: largest No

---
+++ python_Basics/C09_largest_of_two.py
@@ -0,0 +1,8 @@
+a , b= map (int,input("enter two numbers:").split())
+
+if(a>b):
+    print(f"{a} is greater than {b}")
+elif(a==b):
+    print(f"{a} and {b} are equal")
+else:
+     print(f"{b} is greater than {a}")
